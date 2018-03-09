9ede410236568a9449f496855660689aa61967f0
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2018-03-09 19:42:13 +0100

    Working web examples

--- ws_client.py
+++ ws_client.py
@@ -2,9 +2,14 @@
 
 import asyncio
 import websockets
+import os
+
+port = int(os.environ.get('PORT', '8765'))
+print("Starting client on :%s" % port)
+
 
 async def hello():
-    async with websockets.connect('ws://localhost:8765') as websocket:
+    async with websockets.connect('ws://localhost:%s' % port) as websocket:
         name = input("What's your name? ")
         await websocket.send(name)
         print("> {}".format(name))
--- ws_server.py
+++ ws_server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-
 import asyncio
+
 import websockets
+import os
 
 async def hello(websocket, path):
     name = await websocket.recv()
@@ -11,7 +12,14 @@ async def hello(websocket, path):
     await websocket.send(greeting)
     print("> {}".format(greeting))
 
-start_server = websockets.serve(hello, 'localhost', 8765)
 
+port = int(os.environ.get('PORT','8765'))
+print("Starting server on :%s" % port)
+
+start_server = websockets.serve(hello, 'localhost', port)
 asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+
+try:
+    asyncio.get_event_loop().run_forever()
+except KeyboardInterrupt as e:
+    print("\nShutting down server")
